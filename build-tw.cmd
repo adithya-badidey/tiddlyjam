6962e4109731655b023baac1340c9d29aa773b06
Adithya Badidey <bmadithya@gmail.com>
2021-03-13 21:10:51 -0700

    Initial Commit for TiddlyJam Plugin
    - Added the TiddlyJam Commit

---
+++ build-tw.cmd
@@ -0,0 +1,2 @@
+tiddlywiki --render '[[$:/plugins/adithyab/tiddlyjam/renderer/sitemap]]' 'sitemap.xml' text/plain
+tiddlywiki --rendertiddlers '[tag[$:/plugins/adithyab/tiddlyjam/live]]'  $:/plugins/adithyab/tiddlyjam/renderer/base . text/plain
