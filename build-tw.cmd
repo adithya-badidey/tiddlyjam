81d0bad4f051dc9cb40b5941b8e5274aafb717fa
TJ <thaddeusjiang@gmail.com>
2021-07-05 21:55:56 +0900

    update

--- build-tw.cmd
+++
@@ -1,2 +0,0 @@
-tiddlywiki --render '[[$:/plugins/adithyab/tiddlyjam/renderer/sitemap]]' 'sitemap.xml' text/plain
-tiddlywiki --rendertiddlers '[tag[$:/plugins/adithyab/tiddlyjam/live]]'  $:/plugins/adithyab/tiddlyjam/renderer/base . text/plain
